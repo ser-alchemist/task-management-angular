400d16b5170e83dc0b386fc5b420bad5348f9ff7
Md. Rafi <alchemist.rafi@gmail.com>
2023-12-31 16:44:17 +0600

    stable

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-register',
@@ -13,9 +15,12 @@ export class RegisterComponent implements OnInit {
   isSignUpFailed = false;
   errorMessage = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.router.navigate(['login']);
+    }
   }
 
   onSubmit(): void {
